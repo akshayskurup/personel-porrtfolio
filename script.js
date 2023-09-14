ab29702be1503cb8845cf82455185e06423ff352
Akshay s kurup <70994724+akshayskurup@users.noreply.github.com>
2023-09-14 20:16:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+// Script File
+
+// Home Section Starts
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.nav-links');
+var menuLinks = document.querySelectorAll('.nav-links li a');
+
+menuBtn.addEventListener('click', activeClass);
+
+function activeClass(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+}
+
+for(i = 0; i < menuLinks.length; i++){
+	menuLinks[i].addEventListener('click', menuItemClicked);
+}
+
+function menuItemClicked(){
+	menuBtn.classList.remove('active');
+	menu.classList.remove('active');
+}
+
+var homeSection = document.querySelector('.home');
+window.addEventListener('scroll', scrollFunction);
+window.addEventListener('load', scrollFunction);
+
+function scrollFunction(){
+	if(window.scrollY > 60){
+		homeSection.classList.add('active');
+	}
+	else{
+		homeSection.classList.remove('active');
+	}
+}
+// Home Section Ends 
+
+// Portfolio Section Starts
+var $galleryContainer = $('.gallery').isotope({
+	itemSelector: '.item',
+	layoutMode: 'fitRows'
+})
+
+$('.button-group .button').on('click', function(){
+	$('.button-group .button').removeClass('active');
+	$(this).addClass('active');
+
+	var value = $(this).attr('data-filter');
+	$galleryContainer.isotope({
+		filter: value
+	})
+})
+
+// magnific popup
+$('.gallery').magnificPopup({
+	delegate: '.overlay a',
+	type: 'image',
+	gallery:{
+		enabled: true
+	}
+})
+// Portfolio Section Ends
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const email = document.getElementById("email");
+    email.addEventListener("blur", () => {
+        let regex =
+        /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+        let s = email.value;
+        if (regex.test(s)) {
+            email.classList.remove("is-invalid");
+            emailError = true;
+        } else {
+            email.classList.add("is-invalid");
+            emailError = false;
+        }
+    });
+  */
